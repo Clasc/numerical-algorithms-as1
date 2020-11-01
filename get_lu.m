5c78e5b1cf35906ac5c61cc3cdf216f94793e5ab
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-01 12:35:35 +0100

    add get_lu function

---
+++ get_lu.m
@@ -0,0 +1,2 @@
+function [L,U] = get_lu(A)  validateattributes(A, {"numeric"}, {'square'});  n = rows(A);  L = eye(n);  U = A;    for i = 2:n    for j = 1:i-1      L(i,j) = A(i,j);      U(i,j) = 0;    endfor  endfor  
+endfunction

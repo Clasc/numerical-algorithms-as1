7d3db661a3bc9fbdfb2cd05d3fee2f5e9a4796c6
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-03 14:12:36 +0100

    refactor for loops

--- assignment1.m
+++ assignment1.m
@@ -1,12 +1,12 @@
-n = 50;
+n = 10;
 A = rand(n);
 
 [np_L,np_U] = my_lu(A, n);
 [Res, P] = plu(A, n);
 [L, U] = get_lu(Res);
 
-result_no_pivot = np_L * np_U;
-result_pivot = P.' * L * U;
+result_no_pivot = np_L * np_U
+result_pivot = P.' * L * U
 
 R_pivoting = accuracy(result_pivot, A) 
 R_np_pivoting = accuracy(result_no_pivot, A) 

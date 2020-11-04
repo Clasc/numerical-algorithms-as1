5692d3dc9ae6d07fe6a7778368ef4f0a05c54d50
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-04 17:22:27 +0100

    refactor getlu without loops

--- get_lu.m
+++ get_lu.m
@@ -1,2 +1,2 @@
-function [L,U] = get_lu(A)  validateattributes(A, {"numeric"}, {'square'});  n = rows(A);  L = eye(n);  U = A;    for i = 2:n    for j = 1:i-1      L(i,j) = A(i,j);      U(i,j) = 0;    endfor  endfor  
+function [L,U] = get_lu(A)  validateattributes(A, {"numeric"}, {'square'});    L = tril(A,-1) + eye(rows(A));  U = triu(A);  
 endfunction

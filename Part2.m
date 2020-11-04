8e9e11eaf0afe4a57118ff2c3b2641608be9c1db
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-04 19:01:31 +0100

    check if solveU is correct

--- Part2.m
+++ Part2.m
@@ -1,1 +1,1 @@
-B = [5,8,2; -2,5,5; 9,5,7];b = [5;-6;2]n = length(b)x = solveL(B, b, n)y = solveU(B, b, n);
+B = [5,8,2; -2,5,5; 9,5,7];b = [5;-6;2]n = length(b)[L,U] = get_lu(B);x = solveL(B, b, n)y = solveU(B, b, n)U \ b

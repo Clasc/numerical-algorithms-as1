301f5a593f5935c7466580cf3f97255a3ddf876d
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-07 08:46:04 +0100

    AS1:(c) last version of Part3

--- Part3.m
+++ Part3.m
@@ -1,1 +1,31 @@
-n = 3;A = rand(n);b = rand(n,1)x = linSolve(A,b,n);corrx = A\b;
+n = 3;
+n_s = 5:5:400;
+
+iterations = length(n_s);
+R = zeros(iterations, 1);
+Rr = R;
+RrO = R;
+Rf = R;
+RfO = R;
+
+for i = 1:iterations
+    x = ones(n, 1);
+    A = rand(n);
+    b = rand(n, 1)
+
+    x = linSolve(A, b, n);
+    corrx = A \ b;
+
+    Rr(i) = relative_residual(A, x, b);
+
+    Rf(i) = accuracy(corrx, x);
+
+endfor
+
+figure
+semilogy(n_s, RL, n_s, RU);
+title("Forward error");
+xlabel("matrix size n");
+ylabel("Forware error f");
+grid("on");
+legend('Forward error L', "forward error U", 'Location', 'northwest');

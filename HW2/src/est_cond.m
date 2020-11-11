a8010503716fd3adea1adcf15787a2f2051626ae
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-11 19:51:38 +0100

    HW2: (+) assignment.m (+) est_cond.m type checking (+) magnitude.m

---
+++ HW2/src/est_cond.m
@@ -0,0 +1,2 @@
+function c = est_cond(A)   validateattributes(A, {"numeric"}, {'square'});   
+endfunction
---
+++ HW2/src/magnitude.m
@@ -0,0 +1,2 @@
+function m = magnitude(x, y)  if(!isscalar(x) || !isscalar(y))    error("x or y isn't a scalar");  endif    m = max([x,y]) / min([x,y]);
+endfunction

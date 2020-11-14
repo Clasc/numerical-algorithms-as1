531fbbe3a51ee1ecb94f071538b61c483ba3a967
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-14 16:48:48 +0100

    HW2: (c) get estimation vector by random.

--- HW2/src/get_estimation_vector.m
+++ HW2/src/get_estimation_vector.m
@@ -1,2 +1,3 @@
-# Upper Triangular matrix Rfunction e = get_estimation_vector(R)    [U, S, V] = svd(R);
-endfunction
+# Upper Triangular matrix Ufunction [y,z, opt_ratio] = get_estimation_vector(A)  % z is solution to A z = y   n = columns(A);  opt_ratio = 0;  opts.UT = true;    for i = 1:10    _y = rand(n);    _z = linsolve(A,_y, opts);        new_ratio = norm(_z) / norm(_y);    if(opt_ratio < new_ratio)      opt_ratio = new_ratio;      y = _y;      z = _z;    endif  endfor  
+  endfunction
+  

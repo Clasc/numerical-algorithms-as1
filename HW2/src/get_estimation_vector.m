7b4080839313e415e674d40f8aa93b22aa250ed4
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-15 12:43:58 +0100

    HW2: (c) vectorize random y finder function

--- HW2/src/get_estimation_vector.m
+++ HW2/src/get_estimation_vector.m
@@ -1,3 +1,3 @@
-# Upper Triangular matrix Ufunction [y,z, opt_ratio] = get_estimation_vector(A)  % z is solution to A z = y   n = columns(A);  opt_ratio = 0;  opts.UT = true;    for i = 1:10    _y = rand(n);    _z = linsolve(A,_y, opts);        new_ratio = norm(_z) / norm(_y);    if(opt_ratio < new_ratio)      opt_ratio = new_ratio;      y = _y;      z = _z;    endif  endfor  
-  endfunction
-  
+# A is Upper Triangular matrix U transposedfunction [y,z, opt_ratio] = get_estimation_vector(A, iterations = 30)  % z is solution to A z = y   n = columns(A);  _y = rand(n, iterations);    for i = 1 : iterations    _z(:, i) = A\_y(:, i);    new_ratio(i) = norm(_z(:,i)) / norm(_y(:,i));  endfor  [opt_ratio, index] = max(new_ratio);  y= _y(:,index);  z= _z(:,index);
+endfunction
+

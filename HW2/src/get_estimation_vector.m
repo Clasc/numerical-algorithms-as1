5a35e3976d3d8944a688ddc78d57f4616ce7e171
Christian Lascsak <christian.lascsak7@gmail.com>
2020-11-16 23:58:33 +0100

    HW2: (c) update to norm1 in random generator

--- HW2/src/get_estimation_vector.m
+++ HW2/src/get_estimation_vector.m
@@ -1,3 +1,3 @@
-# A is Upper Triangular matrix U transposedfunction [y,z, opt_ratio] = get_estimation_vector(A, iterations = 30)  % z is solution to A z = y   n = columns(A);  _y = rand(n, iterations);    for i = 1 : iterations    _z(:, i) = A\_y(:, i);    new_ratio(i) = norm(_z(:,i)) / norm(_y(:,i));  endfor  [opt_ratio, index] = max(new_ratio);  y= _y(:,index);  z= _z(:,index);
+# A is Upper Triangular matrix U transposedfunction [w, e] = get_estimation_vector(A, iterations = 30)  % z is solution to A z = y   n = columns(A);  _y = rand(n, iterations);    for i = 1 : iterations    _z(:, i) = A\_y(:, i);    new_ratio(i) = norm(_z(:,i),1) / norm(_y(:,i),1);  endfor  [opt_ratio, index] = max(new_ratio);  e = _y(:,index);  w = _z(:,index);
 endfunction
 

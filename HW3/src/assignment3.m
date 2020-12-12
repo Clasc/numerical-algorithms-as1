9fb9127f86d18254e947e6bd064cd40f020d965f
Christian Lascsak <christian.lascsak7@gmail.com>
2020-12-12 13:05:04 +0100

    (+) add workspace (c) fix vector formats

--- HW3/src/assignment3.m
+++ HW3/src/assignment3.m
@@ -1,7 +1,12 @@
 A = [1,2,4; 2,4,1; 1,2,3];
 b = [2;2;1];
 tol = 2;
-maxit = int64(4);
+maxit = uint32(100);
 x0 = [4;2;2];
 
-r = cg(A, b, tol, maxit, x0);
+[x, iter, res_vec] = cg(A, b, tol, maxit, x0);
+x
+res_vec(iter)
+iter
+
+A\b
--- HW3/src/cg.m
+++ HW3/src/cg.m
@@ -1,11 +1,18 @@
-function [x, iter, r, vec] = cg(A, b, tol, maxit, x0)
+% tol: largest admissible relative residual r.
+% The iteration is terminated either when the residual drops below tol, or when the number of iterations exceeds a specified amount maxit. 
+% The initial guess: vector x0.
+% solution vector x, 
+% iter: number of iterations actually performed
+% residual history in res_vec, where res-vec(i) contains the relative residual after iteration i−1 and res vec(1) contains the relative residual of the initial guess.
+
+function [x, iter, res_vec] = cg(A, b, tol, maxit, x0)
     validateattributes(A, {"numeric"}, {'square'});
 
     validateattributes(b, {"numeric"}, {'column'});
     validateattributes(x0, {"numeric"}, {'column'});
 
     validateattributes(tol, {"numeric"}, {'scalar'});
-    validateattributes(maxit, {"int64"}, {'scalar'});
+    validateattributes(maxit, {"uint32"}, {'scalar'});
 
     [n, ~] = size(A);
     if(length(x0)!=n || length(b) != n)
@@ -13,26 +20,31 @@ function [x, iter, r, vec] = cg(A, b, tol, maxit, x0)
     endif
 
     % Folien Sparse Linear Systems S.31
-    r(1) = b - A * x0;
-    s(1) = r(1);
+    r(:,1) = b - (A * x0);
+    r(:,1)' * r(:,1);
+    s_k = r(:,1);
+    x = x0;
     
-    for k = 1:n
-        temp_prod = A * s(k);
+    for k = 1:maxit
+        iter = k;
+        temp_prod = A * s_k;
         
         % Finde von xk in Richtung sk | dk  den Ort xk+1
-        a(k) = r(k)' * r(k) / (s(k)' * temp_prod);
+        a_k = (r(:,k)' * r(:,k)) / (s_k' * temp_prod);
         % aktualisiere Lösung von x E(k) und Residuum
-        x(k+1) = x(k) + a(k) * s(k);
-        r(k+1) = r(k) + a(k) * temp_prod;
+        x = x + a_k * s_k;
+        r(:, k+1) = r(:,k) + a_k * temp_prod;
 
         % korrigiere suchrichtung s 
-        beta_k = (r(k+1)' * r(k+1)) / (r(k)' * r(k));
-        s(k+1) = r(k+1) + beta_k * s(k);
+        beta_k = (r(:,k+1)' * r(:,k+1)) / (r(:,k)' * r(:,k));
+        s_k = r(:,k+1) + beta_k * s_k;
 
         % bis residuum ist kleiner als Toleranz
-        if (norm(r(k+1),1) < tol)
-            iter = k;
+        residual = norm(r(:,k+1),1);
+        if (residual < tol || k >= maxit)
             break;
         endif
     endfor 
+
+    res_vec = r;
 endfunction

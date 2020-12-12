e7476ed8169390ba3044d280b7e924cbdff51f37
Christian Lascsak <christian.lascsak7@gmail.com>
2020-12-12 15:54:00 +0100

    (c) check for expected result correctly

--- HW3/src/assignment3.m
+++ HW3/src/assignment3.m
@@ -1,13 +1,13 @@
 A = [1,2,4; 2,4,1; 1,2,3];
 b = [2;2;1];
-tol = 1;
-maxit = uint32(50);
+tol = 0.5;
+maxit = uint32(23);
 x0 = [4;2;2];
-x0 = A\b;
 
 [x, iter, res_vec] = cg(A, b, tol, maxit, x0);
 
 last_residual = res_vec(:, iter)
 iter
 x
-expected_x = A\b
+correct_x = A\b
+expcted_cg_x =  cgs(A,b)

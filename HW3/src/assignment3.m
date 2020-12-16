1b557a2d12c10c21a54f661872ee737e4c31166a
Christian Lascsak <christian.lascsak@anecon.com>
2020-12-16 16:51:57 +0100

    plot s3 correctly without ichol

--- HW3/src/assignment3.m
+++ HW3/src/assignment3.m
@@ -1,12 +1,18 @@
 %plot_cg_v_pcg("nos6.mtx", 'jacobi');
-plot_cg_tol_relres("nos6.mtx", 'jacobi');
-%plot_process_time("nos6.mtx", 'jacobi');
+% plot_cg_tol_relres("nos6.mtx", 'jacobi');
+plot_process_time("nos6.mtx", 'jacobi');
 
 %plot_cg_v_pcg("nos6.mtx", 'ichol');
-plot_cg_tol_relres("nos6.mtx",  'ichol');
-%plot_process_time("nos6.mtx", 'ichol');
+% plot_cg_tol_relres("nos6.mtx",  'ichol');
+plot_process_time("nos6.mtx", 'ichol');
 
-%plot_all("nos6.mtx");
+%plot_cg_v_pcg("nos6.mtx", 'ichol.ict');
+%plot_cg_tol_relres("nos6.mtx",  'ichol.ict');
+plot_process_time("nos6.mtx", 'ichol.ict');
 
+
+plot_all("nos6.mtx");
+plot_all("nos5.mtx");
+p_s3();
 
 
--- HW3/src/p_s3.m
+++ HW3/src/p_s3.m
@@ -1,7 +1,7 @@
-function plot_all(file)
-  [A, rows, cols, entries] = mmread(file);
-  b = ones(rows,1);
-  x0 = rand(rows,1);
+function p_s3()
+  [A, rows, cols, entries] = mmread("s3rmt3m3.mtx");
+  b = rand(rows,1);
+  x0 = ones(rows,1);
   tol = 1e-6;
   maxit = uint32(rows);
    
@@ -10,21 +10,17 @@ function plot_all(file)
   [M1,M2] = get_precondition(A, "jacobi", rows);
   [~, iter1, res_vec1] = p_cg(A, b, tol, maxit, x0, M1, M2);
   
-  [M1,M2] = get_precondition(A, "ichol", rows);
-  [~, iter2, res_vec2] = p_cg(A, b, tol, maxit, x0, M1, M2);
-  
   [M1,M2] = get_precondition(A, "ichol.ict", rows);
-  [~, iter3, res_vec3] = p_cg(A, b, tol, maxit, x0, M1, M2);
+  [~, iter3, res_vec2] = p_cg(A, b, tol, maxit, x0, M1, M2);
   
    figure
    hold on;
-   title(strcat("comparing cg and pcg on file: ", file));
+   title("comparing cg and pcg on file: s3rmt3m3");
    semilogy(res_vec_cg);
    semilogy(res_vec1);
    semilogy(res_vec2);
-   semilogy(res_vec3);
    xlabel("Iteration");
    ylabel("Relative residual");
    grid("on");
-   legend("without precondition", "jacobi", "ichol", "icol with threshold");
+   legend("without precondition", "jacobi", "icol with threshold");
 endfunction

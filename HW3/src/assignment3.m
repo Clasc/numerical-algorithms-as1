3d13f49bcb4e8f2024e2b47b7d0bb9421398e671
Christian Lascsak <christian.lascsak7@gmail.com>
2020-12-16 14:00:23 +0100

    add ichol and comparison between my and internal pcg

--- HW3/src/assignment3.m
+++ HW3/src/assignment3.m
@@ -1,1 +1,9 @@
-%plot_cg_v_pcg("nos6.mtx", 'jacobi');%plot_cg_tol_relres("nos6.mtx", 'jacobi');plot_process_time("nos6.mtx", 'jacobi');
+plot_cg_v_pcg("nos6.mtx", 'jacobi');
+%plot_cg_tol_relres("nos6.mtx", 'jacobi');
+%plot_process_time("nos6.mtx", 'jacobi');
+
+plot_cg_v_pcg("nos6.mtx", 'ichol');
+%plot_cg_tol_relres("nos6.mtx",  'ichol');
+%plot_process_time("nos6.mtx", 'ichol');
+
+
